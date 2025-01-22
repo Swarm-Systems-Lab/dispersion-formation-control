f12d595dfbe689870814c43c3d9ad29b4123c174
jesusBV20 <rema03@hotmail.es>
2025-01-22 16:40:33 +0100

    Update test_dep.sh

--- notebooks/_test_dep.sh
+++ notebooks/_test_dep.sh
@@ -1,18 +1,21 @@
+#!/bin/bash
+
 # Create virtual environment
 python3 -m venv venv
 
 # Activate virtual environment
 source venv/bin/activate
 
+if [ $? -ne 0 ]; then
+    echo "Failed to activate the virtual environment. Exiting..."
+    exit 1
+fi
+
 # Install dependencies
-python3 install.py
+python3 ../install.py
 
 # Run the project to test
 python3 _requirements.py
-
-# If any modules are missing, install them and update requirements.txt
-# pip install missing_module
-# pip freeze > requirements.txt
 
 # Deactivate virtual environment when done
 deactivate
